1fbfe042f1cef6e9bac32e7bd337e491e5da1a7e
SherryChan <cc4cy@virginia.edu>
2018-09-26 23:42:13 -0400

    Templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -11,3 +11,7 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	#Variable for our query set
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post})
