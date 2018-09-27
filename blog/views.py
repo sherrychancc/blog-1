9a15837ec3798cf38ca4cf711a0016fec6209d3b
SherryChan <cc4cy@virginia.edu>
2018-09-26 21:49:34 -0400

    Posts visibile

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+#. means this directory
+#Take the actual blog posts from Posts (QuerySet) into the view
 
 # Create your views here.
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#Variable for our query set
+	return render(request, 'blog/post_list.html', {'posts':posts})
